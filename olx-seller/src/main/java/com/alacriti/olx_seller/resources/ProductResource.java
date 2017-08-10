cd0c0b61c2604ab14aaf0450777a587b971ab3b4
mahammadirshadk <mahammadirshad.kutagolla@alacriti.com>
2017-08-10 12:37:05 +0530

    added a method for search products

--- olx-seller/src/main/java/com/alacriti/olx_seller/resources/ProductResource.java
+++ olx-seller/src/main/java/com/alacriti/olx_seller/resources/ProductResource.java
@@ -2,22 +2,26 @@ package com.alacriti.olx_seller.resources;
 
 import java.util.ArrayList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.alacriti.olx_seller.delegate.ProductDelegate;
 import com.alacriti.olx_seller.model.vo.ProductVO;
+import com.alacriti.olx_seller.model.vo.SearchProdutVO;
 
 @Path("products")
+@Produces(MediaType.APPLICATION_JSON)
 public class ProductResource {
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getProducts(){
-		ArrayList<ProductVO> products = new ArrayList<ProductVO>();
+		ArrayList<ProductVO> products;
 		ProductDelegate productDelegate = new ProductDelegate();
 		products=productDelegate.getProducts();
 		return Response.status(200).entity(products).build();
@@ -25,7 +29,6 @@ public class ProductResource {
 	}
 	@GET
 	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getProductById(@PathParam("id") int id){
 		ProductVO productVO = new ProductVO();
 		productVO.setProduct_id(id);
@@ -34,5 +37,26 @@ public class ProductResource {
 		return Response.status(200).entity(productVO).build();
 		
 	}
-	
+	/*@POST
+	@Path("/search")
+	public Response getProdctsBySearch(
+			@QueryParam("title") String title,
+			@QueryParam("catId") int catId
+			){
+		System.out.println(title+": "+catId);
+		ArrayList<ProductVO> products=null;
+		ProductDelegate productDelegate = new ProductDelegate();
+		products=productDelegate.getProducts(title,catId);
+		return Response.status(200).entity(products).build();
+	}*/
+	@POST
+	@Path("/search")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response getProdctsBySearch(SearchProdutVO searchProdutVO){
+		System.out.println(searchProdutVO.getTitle()+": "+searchProdutVO.getCatId());
+		ArrayList<ProductVO> products=null;
+		ProductDelegate productDelegate = new ProductDelegate();
+		products=productDelegate.getProducts(searchProdutVO);
+		return Response.status(200).entity(products).build();
+	}
 }
